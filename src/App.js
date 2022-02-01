967a828b3962a20587f46027933e6dda67866395
Tyler Yates <tyates907@gmail.com>
2022-01-31 19:25:22 -0800

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./screens/HomePage";
+import NoMatch from "./screens/NoMatch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" >
+            <Route index element={<HomePage logging = {log} userObject = {userObj}/>}/>
+          </Route>
+          <Route path="*" element={<NoMatch/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
