7783b0ef85d4305d07e0ba4c6f1b990ad39da0ba
Tyler Yates <tyates907@gmail.com>
2022-02-01 14:02:59 -0800

    minor style edits

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -9,14 +9,16 @@ export default function LoginPage() {
 		event.preventDefault();
 		console.log(username);
 		console.log(password);
+		setUsername("");
+		setPassword("");
 	}
 	return (
 	<div>
 		<Navbar/>
 		<form className="customerLogin" onSubmit={(e) => loginHandler(e)}>
-			<label>Username</label>
+			<label>Username:</label>
 			<input type="text" value={username} onChange={(el) => {setUsername(el.target.value);}}></input>
-			<label>Password</label>
+			<label>Password:</label>
 			<input type="password" value={password} onChange={(el) => setPassword(el.target.value)}></input>
 			<input className='submit' type="submit"></input>
 		</form>
